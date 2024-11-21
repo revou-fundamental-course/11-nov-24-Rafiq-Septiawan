0fcffe8f35966aef0cb6e8ef72d57be8bafc94a9
Rafiq Septiawan <septiawanaja2004@gmail.com>
2024-11-21 10:02:00 +0700

    update all

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-// ini JavaScript
+// Ini JavaScript //
+function validateForm() {
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const interest = document.getElementById('interest').value;
+
+    if (!name || !email || !interest) {
+        alert("Semua kolom wajib diisi.");
+        return false;
+    }
+
+    alert("Formulir berhasil dikirimkan!");
+    return true;
+}
+
+
+//Banner AutoSlide//
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    const dots = document.querySelectorAll('.dot');
+
+    slides.forEach(slide => slide.style.display = 'none');
+
+    dots.forEach(dot => dot.classList.remove('active'));
+
+    slideIndex++;
+    if (slideIndex > slides.length) slideIndex = 1;
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('active');
+
+
+    setTimeout(showSlides, 3000);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlides();
+});
